d801262b5ac232ad0898b5972e948047fbf1dd65
Denis Matveev <matveev@magora-systems.com>
2016-11-23 16:55:43 +0700

    fix

--- Example/Pods/Target Support Files/MAGMatveevReusable/MAGMatveevReusable-umbrella.h
+++ Example/Pods/Target Support Files/MAGMatveevReusable/MAGMatveevReusable-umbrella.h
@@ -1,5 +1,12 @@
 #import <UIKit/UIKit.h>
 
+#import "MAGCommonDefines.h"
+#import "NSObject+MAGMore.h"
+#import "UIView+MAGMore.h"
+#import "UINavigationBar+MAGMore.h"
+#import "UITextView+MAGMore.h"
+#import "UIViewController+MAGMore.h"
+
 #import "MAGMatveevReusable.h"
 
 FOUNDATION_EXPORT double MAGMatveevReusableVersionNumber;
