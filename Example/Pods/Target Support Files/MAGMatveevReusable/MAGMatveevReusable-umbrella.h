5218e20acaa93864ca88e45110f189998de562e6
Denis Matveev <matveev@magora-systems.com>
2017-01-30 22:33:08 +0700

    again fixxxx

--- Example/Pods/Target Support Files/MAGMatveevReusable/MAGMatveevReusable-umbrella.h
+++ Example/Pods/Target Support Files/MAGMatveevReusable/MAGMatveevReusable-umbrella.h
@@ -1,18 +1,28 @@
 #ifdef __OBJC__
 #import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
 #endif
 
 #import "MAGCommonDefines.h"
+#import "MAGDrawing.h"
+#import "MAGFrame.h"
+#import "MAGMaterializerView.h"
+#import "MAGMatveevReusable.h"
+#import "NSArray+MAGMore.h"
+#import "NSDictionary+MAGMore.h"
 #import "NSObject+MAGMore.h"
 #import "UINavigationBar+MAGMore.h"
 #import "UITextView+MAGMore.h"
-#import "UIView+MAGMore.h"
 #import "UIView+MAGAnimation.h"
-#import "MAGFrame.h"
+#import "UIView+MAGMore.h"
 #import "UIViewController+MAGMore.h"
-#import "MAGDrawing.h"
-
-#import "MAGMatveevReusable.h"
 
 FOUNDATION_EXPORT double MAGMatveevReusableVersionNumber;
 FOUNDATION_EXPORT const unsigned char MAGMatveevReusableVersionString[];
