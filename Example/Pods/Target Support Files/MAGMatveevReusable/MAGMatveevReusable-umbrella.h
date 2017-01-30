96473df8c8ff73a0308e83d8c69f2a058af4cd76
Denis Matveev <matveev@magora-systems.com>
2017-01-30 22:04:12 +0700

    Revert "fixes"
    This reverts commit 3e48a922ce6ae1c7795cac7b6c2cec4c42512411.

--- Example/Pods/Target Support Files/MAGMatveevReusable/MAGMatveevReusable-umbrella.h
+++ Example/Pods/Target Support Files/MAGMatveevReusable/MAGMatveevReusable-umbrella.h
@@ -3,19 +3,21 @@
 #endif
 
 #import "MAGCommonDefines.h"
-#import "MAGDrawing.h"
-#import "MAGFrame.h"
-#import "MAGMaterializerView.h"
-#import "MAGMatveevReusable.h"
-#import "NSArray+MAGMore.h"
-#import "NSDictionary+MAGMore.h"
 #import "NSObject+MAGMore.h"
-#import "UIDevice+Reachability.h"
 #import "UINavigationBar+MAGMore.h"
 #import "UITextView+MAGMore.h"
-#import "UIView+MAGAnimation.h"
 #import "UIView+MAGMore.h"
+#import "UIView+MAGAnimation.h"
+#import "MAGFrame.h"
 #import "UIViewController+MAGMore.h"
+#import "MAGDrawing.h"
+
+#import "NSArray+MAGMore.h"
+#import "NSDictionary+MAGMore.h"
+#import "UIDevice+Reachability.h"
+#import "MAGMaterializerView.h"
+
+#import "MAGMatveevReusable.h"
 
 FOUNDATION_EXPORT double MAGMatveevReusableVersionNumber;
 FOUNDATION_EXPORT const unsigned char MAGMatveevReusableVersionString[];
